ec5fa8b6bf789934c6bf2c0e95d598987d9c04e8
newuser <Yahia.fara24@gmail.com>
2026-07-12 16:30:57 +0300

    added ui

---
+++ lib/designTokens/design_tokens.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class DesignTokens {
+  static Color primary =Color(0xffF65A0E);
+  static Color deepOrange= Color(0xffC23E00);
+  static Color ember = Color(0xffC23E00);
+  static Color cream = Color(0xffFFF8F2);
+  static Color ink =Color(0xff2A1408);
+}
